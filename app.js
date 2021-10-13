cbe92bf2a8730c424f409756762b9f2bbe2b8ac6
unknown <rajeev.sahu85@gmail.com>
2021-10-13 09:06:59 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const form = document.querySelector('.top-banner form');
+const input = document.querySelector('.top-banner input');
+const msg = document.querySelector('.top-banner .msg');
+const list=document.querySelector('.ajax-section .cities');
+
+const apiKey = "7b46f565a7bf8caf3a8bf1fc5cae6cce";
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let inputVal=input.value;
+
+
+
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(data=>data.json())
+        .then(data=>{
+            // console.log(data);
+            const { main, name, sys, weather } = data;
+            const icon=`https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+                weather[0]["icon"]}.svg`;
+
+                const li = document.createElement("li");
+                li.classList.add("city");
+                const markup=`
+                <h2 class="city-name">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+                </h2>
+                <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+                <figure>
+                   <img class="city-icon" src="${icon}" alt="${weather[0]["description"]
+                }">
+                <figcaption>${weather[0]["description"]}</figcaption>
+                </figure>
+                `;
+                li.innerHTML = markup;
+                list.appendChild(li);
+        })
+        .catch(()=>{
+            msg.textContent="Please search for a valid city 😩";
+        })
+
+        msg.textContent = "";
+        form.reset();
+        input.focus();
+
+});
+
